82022335abaf4d4bc47c1ae897db76304ac335a8
Erik Swanson <theerikswanson@gmail.com>
2016-06-17 16:19:19 -0400

    Add pandoc compilation from markdown to html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 var gulp = require('gulp');
+var pandoc = require('gulp-pandoc');
 
 var config = {
     paths: {
         html: './src/html/*.html',
+        articles: './src/articles/*.md',
         images: './src/images/*',
         dist: './static',
     }
-}
+};
+
+var pandoc_articles_config = [
+    '--smart',
+    '-s',
+]
 
 gulp.task('html', function () {
     gulp.src(config.paths.html, {base: './src'})
         .pipe(gulp.dest(config.paths.dist))
-})
+});
 
-gulp.task('default', ['html']);
+gulp.task('articles', function () {
+    gulp.src(config.paths.articles, {base: './src'})
+        .pipe(pandoc({
+            from: 'markdown',
+            to: 'html5',
+            ext: '.html',
+            args: pandoc_articles_config,
+        }))
+        .pipe(gulp.dest(config.paths.dist))
+});
+
+gulp.task('default', ['html', 'articles']);
---
+++ src/articles/test.md
@@ -0,0 +1,17 @@
+Testing 123
+===========
+
+This is a markdown test
+
+- Testing
+- one
+- two
+- three
+
+## Can it get it right?
+
+I have no idea. Here's a link to [Google](www.google.com).
+
+```
+import this
+```
