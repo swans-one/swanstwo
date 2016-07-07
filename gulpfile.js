5387420fff88104d53c6c879d0fad01df79f0ec7
Erik Swanson <theerikswanson@gmail.com>
2016-07-07 15:03:15 -0400

    Gulp commands for index pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,23 @@
 var gulp = require('gulp');
 var pandoc = require('gulp-pandoc');
 var sass = require('gulp-sass');
+var debug = require('gulp-debug');
 
 var config = {
     paths: {
         html: './src/html/**/*.html',
         css: './src/css/**/*.css',
         sass: './src/css/**/*.scss',
-        articles: './src/articles/**/*.md',
+        articles: ['./src/writings/**/*.md', '!./src/**/index.md'],
+        index_pages: './src/**/index.md',
         media: './src/media/**/*',
         favicon: './src/html/favicon.ico',
         fonts: './src/fonts/**/*',
         dist: './static',
     },
     templates: {
-        article_template: './src/templates/article.html'
+        article_template: './src/templates/article.html',
+        index_pages_template: './src/templates/index_page.html',
     },
 };
 
@@ -25,6 +28,7 @@ var default_tasks = [
     'css',
     'sass',
     'articles',
+    'index_pages',
     'media',
     'favicon',
     'fonts',
@@ -33,6 +37,11 @@ var default_tasks = [
 var pandoc_articles_config = [
     '--smart',
     '--template=' + config.templates.article_template,
+];
+
+var pandoc_index_pages_config = [
+    '--smart',
+    '--template=' + config.templates.index_pages_template,
 ];
 
 gulp.task('html', function () {
@@ -73,6 +82,17 @@ gulp.task('articles', function () {
             to: 'html5',
             ext: '.html',
             args: pandoc_articles_config,
+        }))
+        .pipe(gulp.dest(config.paths.dist + '/html'))
+});
+
+gulp.task('index_pages', function () {
+    gulp.src(config.paths.index_pages, {base: './src'})
+        .pipe(pandoc({
+            from: 'markdown',
+            to: 'html5',
+            ext: '.html',
+            args: pandoc_index_pages_config,
         }))
         .pipe(gulp.dest(config.paths.dist + '/html'))
 });
